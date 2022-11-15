c8b94799f7360107cb7246d28332fa9ad0d4e7cf
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2022-11-14 19:52:03 -0800

    createpost.js

--- controllers/api/createPost.js
+++ controllers/api/createPost.js
@@ -4,30 +4,48 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
-    res.render('createPost');
+    res.render('createPost', {loggedIn: req.session.loggedIn});
 });
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
+//     try {
+//         const dbNewPost = await Post.create({
+//             userPost: req.body.post,
+//             userLink: req.body.link,
+
+//         });
+//         if (!newPost) {
+//             res.status(404).json({ message: 'Email cannot be found!'});
+//             return
+//         }
+//         req.session.save(() => {
+//             req.session.loggedIn = true;
+//             console.log(req.session.cookie);
+//             res.status(200).json({ message: 'Logged in!'})
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+router.post("/", async (req, res) => {
     try {
-        const userData = await Post.create({});
-        if (!userData) {
-            res.status(404).json({ message: 'Email cannot be found!'});
-            return
-        }
-        const validatePass = await bcrypt.compare(
-            req.body.password,
-            userData.password
-        );
-        if (!validatePass){
-            res.status(404).json({ message: 'Password incorrect!'});
-            return
-        }
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            console.log(req.session.cookie);
-            res.status(200).json({ message: 'Logged in!'})
+        const dbPostData = await Post.create({
+            post_body: req.post.body,
+            post_links: req.post.link,
+            include: [
+                {
+                    model: User,
+                    id: req.session.id,
+                    username: req.session.username,
+                    email: req.session.email,
+                },
+            ],
         });
+       
+        console.log(dbPostData)
+        res.status(200).json(dbPostData)
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 })
---
+++ public/js/createPost.js
@@ -0,0 +1,22 @@
+const posthandler = async ( event )  => {
+    event.preventDefault();
+
+const createPost = document.querySelector("#post-input").value.trim();
+const createLink = document.querySelector("#link-input").value.trim();
+console.log({createPost, createLink})
+if (createPost) {
+    const response = await fetch ('/createPost', {
+        method: 'POST',
+        body: JSON.stringify({createPost, createLink}),    
+        headers: {'Content-Type': 'application/json'}
+    });
+    if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed Post.');
+      }
+ } };
+
+    const postSubmitBtn = document.querySelector('.post')
+
+    postSubmitBtn.addEventListener('submit', posthandler);
